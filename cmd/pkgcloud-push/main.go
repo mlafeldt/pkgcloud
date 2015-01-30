a3f85ba6f47fe2fb04cb5f75fe234d2944951ad3
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2015-01-30 19:01:16 +0100

    Push packages in parallel

--- cmd/pkgcloud-push/main.go
+++ cmd/pkgcloud-push/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/mlafeldt/pkgcloud"
 )
@@ -16,13 +17,19 @@ func main() {
 		log.Fatal("Usage: pkgcloud-push user/repo/distro/version /path/to/packages")
 	}
 
-	// TODO: upload packages in parallel
 	client := pkgcloud.NewClient("")
 	target := flag.Args()[0]
+
+	var wg sync.WaitGroup
 	for _, name := range flag.Args()[1:] {
-		fmt.Printf("Pushing %s to %s ...\n", name, target)
-		if err := client.PushPackage(target, name); err != nil {
-			log.Fatal(err)
-		}
+		wg.Add(1)
+		go func(name string) {
+			defer wg.Done()
+			fmt.Printf("Pushing %s to %s ...\n", name, target)
+			if err := client.PushPackage(target, name); err != nil {
+				log.Fatal(err)
+			}
+		}(name)
 	}
+	wg.Wait()
 }
