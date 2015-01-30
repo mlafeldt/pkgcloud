36b891374c683d884bf62700c14c5b5b35872a07
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2015-01-30 15:48:51 +0100

    Allow pushing multiple packages

--- cmd/pkgcloud-push/main.go
+++ cmd/pkgcloud-push/main.go
@@ -19,6 +19,7 @@ func pushPackage(dest, source string) error {
 	s := strings.Split(dest, "/")
 	user, repo, distro, version := s[0], s[1], s[2], s[3]
 
+	// TODO: support other formats like .rpm
 	distroVersionName := distro + "/" + version
 	distroVersionID, ok := debDistros[distroVersionName]
 	if !ok {
@@ -49,6 +50,7 @@ func pushPackage(dest, source string) error {
 		return err
 	}
 
+	// TODO: unmarshal JSON to get error
 	if resp.StatusCode != http.StatusCreated {
 		msg := fmt.Sprintf("HTTP error: %s\nHTTP body: %s\n",
 			resp.Status, body)
@@ -63,14 +65,14 @@ func main() {
 
 	flag.Parse()
 	if flag.NArg() < 2 {
-		log.Fatal("Usage: pkgcloud push user/repo/distro/version /path/to/packages")
+		log.Fatal("Usage: pkgcloud-push user/repo/distro/version /path/to/packages")
 	}
 
 	dest := flag.Args()[0]
-	source := flag.Args()[1]
-
-	fmt.Printf("Pushing %s to %s ...\n", source, dest)
-	if err := pushPackage(dest, source); err != nil {
-		log.Fatal(err)
+	for _, source := range flag.Args()[1:] {
+		fmt.Printf("Pushing %s to %s ...\n", source, dest)
+		if err := pushPackage(dest, source); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
