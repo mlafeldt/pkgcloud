c5f7bbe5791cca3b1abe91f3ade778ef1008d4e0
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2015-01-30 15:00:51 +0100

    First working package upload

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var serviceURL = "https://packagecloud.io"
@@ -16,61 +18,57 @@ func init() {
 	token = os.Getenv("PACKAGECLOUD_TOKEN")
 }
 
-func sendRequest(endpoint string, v interface{}) error {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", serviceURL+endpoint, nil)
+func pushPackage(dest, source string) error {
+	s := strings.Split(dest, "/")
+	user, repo, distro, version := s[0], s[1], s[2], s[3]
+
+	distroVersionID := debDistros[distro+"/"+version]
+
+	fmt.Println(user, repo, distro, version, distroVersionID)
+
+	endpoint := fmt.Sprintf("%s/api/v1/repos/%s/%s/packages.json",
+		serviceURL, user, repo)
+	extraParams := map[string]string{
+		"package[distro_version_id]": strconv.Itoa(distroVersionID),
+	}
+	request, err := NewFileUploadRequest(endpoint, extraParams,
+		"package[package_file]", source)
 	if err != nil {
 		return err
 	}
-	req.SetBasicAuth(token, "")
+	request.SetBasicAuth(token, "")
 
-	resp, err := client.Do(req)
+	client := &http.Client{}
+	resp, err := client.Do(request)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("HTTP error: %s", resp.Status)
-	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
 
-	return json.Unmarshal(body, v)
-}
+	fmt.Println(string(body))
 
-type distroVersion struct {
-	DisplayName   string `json:"display_name"`
-	ID            int    `json:"id"`
-	IndexName     string `json:"index_name"`
-	VersionNumber string `json:"version_number"`
-}
+	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("HTTP error: %s", resp.Status)
+	}
 
-type distro struct {
-	DisplayName string          `json:"display_name"`
-	IndexName   string          `json:"index_name"`
-	Versions    []distroVersion `json:"versions"`
+	return nil
 }
-
-type distros map[string][]distro
 
 func main() {
-	var distros distros
-	err := sendRequest("/api/v1/distributions.json", &distros)
-	if err != nil {
-		log.Fatal(err)
+	flag.Parse()
+	if flag.NArg() < 2 {
+		log.Fatal("missing args")
 	}
-	//log.Printf("%v\n", distros)
 
-	for _, format := range []string{"deb", "dsc", "rpm"} {
-		fmt.Println(format)
-		for _, d := range distros[format] {
-			for _, v := range d.Versions {
-				fmt.Printf("\"%s/%s\": %d,\n", d.IndexName, v.IndexName, v.ID)
-			}
-		}
+	dest := flag.Args()[0]
+	source := flag.Args()[1]
+
+	if err := pushPackage(dest, source); err != nil {
+		log.Fatal(err)
 	}
 }
