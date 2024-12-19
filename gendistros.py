afd7b4b2e6a1a06dc0bb5876302a4c77e842fe48
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2024-12-20 00:22:47 +0100

    Fix gendistros.py to use py3

--- gendistros.py
+++ gendistros.py
@@ -1,34 +1,41 @@
 #!/usr/bin/env python
-# Generate Go map of distros supported by Packagecloud API
-# By generating the list once, we save an expensive API call.
-# See https://packagecloud.io/docs/api#resource_distributions
+"""
+ Generate Go map of distros supported by Packagecloud API
+ By generating the list once, we save an expensive API call.
+ See https://packagecloud.io/docs/api#resource_distributions
+"""
 
-import os, sys
-import urllib
+import base64
 import json
+import os
+import sys
+from pathlib import Path
+from urllib.request import Request, urlopen
 
-var = sys.argv[1]
-token = os.environ['PACKAGECLOUD_TOKEN']
+VAR = "supportedDistros"
+if len(sys.argv) > 1:
+    VAR = sys.argv[1]
+token = os.environ["PACKAGECLOUD_TOKEN"]
 
-url = 'https://%s:@packagecloud.io/api/v1/distributions.json' % token
-resp = urllib.urlopen(url)
-data = json.loads(resp.read())
+request = Request("https://packagecloud.io/api/v1/distributions.json")
+b64 = base64.b64encode(bytes(f"{token}:", "ascii"))
+request.add_header("Authorization", f"Basic {b64.decode('utf-8')}")
+with urlopen(request) as resp:
+    data = json.loads(resp.read())
 
 result = {}
 for distros in data.values():
     for d in distros:
-        for v in d['versions']:
-            k = d['index_name']
-            if 'index_name' in v:
-                k = '/'.join([k, v['index_name']])
-            v = v['id']
+        for v in d["versions"]:
+            k = d["index_name"]
+            if "index_name" in v:
+                k = "/".join([k, v["index_name"]])
+            v = v["id"]
             result[k] = v
 
-print '// Generated with %s' % __file__
-print
-print 'package pkgcloud'
-print
-print 'var %s = map[string]int{' % var
-for k, v in sorted(result.items(),key=lambda x:x[1]):
-    print "\t\"%s\": %d," % (k, v)
-print '}'
+print(f"// Generated with {Path( __file__ ).name}")
+print("\npackage pkgcloud\n")
+print(f"var {VAR} = map[string]int{{")
+for k, v in sorted(result.items(), key=lambda x: x[1]):
+    print(f'\t"{k}": {v},')
+print("}")
